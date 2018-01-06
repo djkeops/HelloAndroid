2aab991f8aec61e74ef8bcb6fcf1ee53f7cf05ad
djkeops <djkeopscris@yahoo.com>
2018-01-06 19:24:44 +0200

    Some improvements

--- app/src/main/java/com/example/android/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -15,9 +15,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void signup(View view) {
-        Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://auth.udacity.com/sign-up"));
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://auth.udacity.com/sign-up"));
         if (browserIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(browserIntent);
+        }
+    }
+
+    public void callUdacity(View view){
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + getString(R.string.phone)));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void showAdress(View view){
+
+        Uri gmmIntentUri = Uri.parse("geo:37.395097,-122.097513?q=&z=18(Udacity)");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
         }
     }
 }
